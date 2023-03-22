d0e443383f43cecd9f2d4c2bfef6b0b92bee716e
jaofe <104651266+jaofe@users.noreply.github.com>
2023-03-22 13:19:27 -0300

    Add files via upload

---
+++ interface/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Layout from "./layout/Layout";
+import Cadastro from "./paginas/Cadastro";
+import CadastroLivro from "./paginas/CadastroLivro";
+import Home from "./paginas/Home";
+import LivroDetalhes from "./paginas/LivroDetalhes";
+import Livros from "./paginas/Livros";
+import Login from "./paginas/Login";
+import { UserContextProvider } from "./user-context";
+
+function App() {
+  return (
+    <UserContextProvider>
+      <Layout>
+        <Routes>
+          <Route path={"/"} element={<Home />} exact />
+          <Route path={"/cadastro"} element={<Cadastro />} />
+          <Route path={"/login"} element={<Login />} />
+          <Route path={"/cadastro-livro"} element={<CadastroLivro />} />
+          <Route path={"/livros"} element={<Livros />} />
+          <Route path={"/livros/:livro"} component={LivroDetalhes} />
+        </Routes>
+      </Layout>
+    </UserContextProvider>
+  );
+}
+
+export default App;
