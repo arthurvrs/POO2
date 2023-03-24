8dda8ef8f50278b10f85fe4fc22a0d7389a2aeda
jaofe <104651266+jaofe@users.noreply.github.com>
2023-03-24 18:38:47 -0300

    Add files via upload

--- src/main/java/biblioteca/biblio/LivroImpl.java
+++ src/main/java/biblioteca/biblio/LivroImpl.java
@@ -1,6 +1,11 @@
 package biblioteca.biblio;
 
+import java.time.LocalDate;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +29,26 @@ public class LivroImpl implements MainController<Livro>{
 
         int idint = Integer.parseInt(id);
         return ResponseEntity.ok(biblioteca.buscarLivroId(idint));
+    }
+
+    @PostMapping("/alugar/:{id}/:{username}")
+    public ResponseEntity<?> alugarLivro(@RequestBody LocalDate data, @PathVariable("id") int id, @PathVariable("username") String username) {
+        Cliente cliente = biblioteca.buscarCliente(username);
+
+        for(Livro l: cliente.livrosAlugados) {
+            if(l.isAtrasado()) {
+                ResponseEntity.ok("Devido a existencia de livros atrasados, esta função está indisponivel!");
+            }
+        }
+
+        Livro livro = biblioteca.buscarLivroId(id);
+
+        if (livro.getDisponibilidade()) {
+            cliente.alugarLivro(livro);
+            livro.dataDevolucao = data;
+            return ResponseEntity.ok("ok");
+        } else {
+            return ResponseEntity.ok("Livro indisponivel!");
+        }
     }
 }
