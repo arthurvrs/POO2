98bf875ef3df2d3cadc0b2281ca4ab5784630d22
jaofe <104651266+jaofe@users.noreply.github.com>
2023-03-29 10:24:36 -0300

    Add files via upload

--- interface/src/layout/BarraDeNavegacao.js
+++ interface/src/layout/BarraDeNavegacao.js
@@ -32,101 +32,125 @@ function BarraDeNavegacao() {
   const busca = (event) => {
     event.preventDefault();
     const searchData = buscaInputRef.current.value;
-    navigate(`/busca/${searchData}`)
+    navigate(`/busca/${searchData}`);
   };
 
   if (UserInfo.type === "user") {
     return (
       <header className={classes.header}>
-        <Link to="/">Biblioteca</Link>
-        <div className={classes.dropdown}>
-          <button className={classes.dropbtn}>Menu</button>
-          <div className={classes.dropdowncontent}>
-            <Link to="/">Home</Link>
-            <Link to="/livros">Livros</Link>
-            <Link to="/livros-alugados">Livros Alugados</Link>
-            <Link to="/livros-devolvidos">Escrever Review</Link>
-            <Link to="/multas">Multas</Link>
+        <div>
+          <Link to="/">Biblioteca</Link>
+          <div className={classes.dropdown}>
+            <button className={classes.dropbtn}>Menu</button>
+            <div className={classes.dropdowncontent}>
+              <Link to="/">Home</Link>
+              <Link to="/livros">Livros</Link>
+              <Link to="/livros-alugados">Livros Alugados</Link>
+              <Link to="/livros-devolvidos">Escrever Review</Link>
+              <Link to="/multas">Multas</Link>
+            </div>
           </div>
         </div>
-        <form onSubmit={busca}>
-          <input type="text" ref={buscaInputRef}/>
-          <button type="submit" className={classes.button}>Busca</button>
-        </form>
-        <nav>
-          <ul>
-            <li>
-              <Link to={"/usuario"}>{UserInfo.username}</Link>
-            </li>
-            <li>
-              <Link to="/" onClick={sair}>
-                Sair
-              </Link>
-            </li>
-          </ul>
-        </nav>
+        <div>
+          <form onSubmit={busca}>
+            <input type="text" ref={buscaInputRef} />
+            <button type="submit" className={classes.button}>
+              Busca
+            </button>
+          </form>
+        </div>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to={"/usuario"}>{UserInfo.username}</Link>
+              </li>
+              <li>
+                <Link to="/" onClick={sair}>
+                  Sair
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </header>
     );
   } else if (UserInfo.type === "admin") {
     return (
       <header className={classes.header}>
-        <Link to="/">Biblioteca</Link>
-        <div className={classes.dropdown}>
-          <button className={classes.dropbtn}>Menu</button>
-          <div className={classes.dropdowncontent}>
-            <Link to="/">Home</Link>
-            <Link to="cadastro-admin">Cadastrar Administrador</Link>
-            <Link to="/cadastro-livro">Cadastrar Livro</Link>
-            <Link to="/livros">Livros</Link>
-            <Link to="/usuarios-atrasados">Usuarios Atrasados</Link>
-            <Link to="/livros-atrasados">Livros Atrasados</Link>
-            <Link to="/teste">Teste</Link>
+        <div>
+          <Link to="/">Biblioteca</Link>
+          <div className={classes.dropdown}>
+            <button className={classes.dropbtn}>Menu</button>
+            <div className={classes.dropdowncontent}>
+              <Link to="/">Home</Link>
+              <Link to="cadastro-admin">Cadastrar Administrador</Link>
+              <Link to="/cadastro-livro">Cadastrar Livro</Link>
+              <Link to="/livros">Livros</Link>
+              <Link to="/usuarios-atrasados">Usuarios Atrasados</Link>
+              <Link to="/livros-atrasados">Livros Atrasados</Link>
+              <Link to="/teste">Teste</Link>
+            </div>
           </div>
         </div>
-        <form onSubmit={busca}>
-          <input type="text" ref={buscaInputRef}/>
-          <button type="submit" className={classes.button}>Busca</button>
-        </form>
-        <nav>
-          <ul>
-            <li>
-              <Link to={"/usuario"}>{UserInfo.username}</Link>
-            </li>
-            <li>
-              <Link to="/" onClick={sair}>
-                Sair
-              </Link>
-            </li>
-          </ul>
-        </nav>
+        <div>
+          <form onSubmit={busca}>
+            <input type="text" ref={buscaInputRef} />
+            <button type="submit" className={classes.button}>
+              Busca
+            </button>
+          </form>
+        </div>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to={"/usuario"}>{UserInfo.username}</Link>
+              </li>
+              <li>
+                <Link to="/" onClick={sair}>
+                  Sair
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </header>
     );
   } else {
     return (
       <header className={classes.header}>
-        <Link to="/">Biblioteca</Link>
-        <div className={classes.dropdown}>
-          <button className={classes.dropbtn}>Menu</button>
-          <div className={classes.dropdowncontent}>
-            <Link to="/">Home</Link>
-            <Link to="/login">Login</Link>
-            <Link to="/cadastro">Cadastrar-se</Link>
+        <div>
+          <Link to="/">Biblioteca</Link>
+          <div className={classes.dropdown}>
+            <button className={classes.dropbtn}>Menu</button>
+            <div className={classes.dropdowncontent}>
+              <Link to="/">Home</Link>
+              <Link to="/login">Login</Link>
+              <Link to="/cadastro">Cadastrar-se</Link>
+            </div>
           </div>
         </div>
-        <form onSubmit={busca}>
-          <input type="text" ref={buscaInputRef}/>
-          <button type="submit" className={classes.button}>Busca</button>
-        </form>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/login">login</Link>
-            </li>
-            <li>
-              <Link to="/cadastro">Cadastro</Link>
-            </li>
-          </ul>
-        </nav>
+        <div>
+          <form onSubmit={busca}>
+            <input type="text" ref={buscaInputRef} />
+            <button type="submit" className={classes.button}>
+              Busca
+            </button>
+          </form>
+        </div>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/login">login</Link>
+              </li>
+              <li>
+                <Link to="/cadastro">Cadastro</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </header>
     );
   }
